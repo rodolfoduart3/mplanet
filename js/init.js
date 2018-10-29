7ffe042331d626ad5bd29c3a33556c40599d5783
Rodolfo Duarte <rodolfo.duarte@bistwobis.com.br>
2018-10-29 08:40:56 -0200

    Fixes on Fontello Icons / Adding Slick Slider files

--- js/init.js
+++ js/init.js
@@ -3,6 +3,15 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    
+    $('.musics-description').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+		
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
