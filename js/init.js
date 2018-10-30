793b48319716e1649042105dbfdd2b114be027be
Rodolfo Duarte <rodolfo.duarte@bistwobis.com.br>
2018-10-30 13:56:35 -0200

    Style changes - SlickSlider

--- js/init.js
+++ js/init.js
@@ -5,13 +5,31 @@
     $('.parallax').parallax();
     
     $('.musics-description').slick({
-      dots: true,
+      arrows: true,
       infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
+      slidesToShow: 3,
+      slidesToScroll: 3,
+  		dots: true,
+      speed: 500,      
+  		responsive: [{
+  		  breakpoint: 1024,
+  		  settings: {
+  			slidesToShow: 3
+  		  }
+  		},{
+  		  breakpoint: 994,
+  		  settings: {
+    			slidesToShow: 2,
+          slidesToScroll: 2
+  		  }
+  		},{
+        breakpoint: 768,
+  		  settings: {
+    			slidesToShow: 1,
+          slidesToScroll: 1
+  		  }
+  		}]
     });
-		
-
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
